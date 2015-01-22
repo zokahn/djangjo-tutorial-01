55ba8fa6753ca0079302d6ae43de2efb20c659de
Bart van den Heuvel <bart@redhat.com>
2015-01-22 15:05:20 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# djangjo-tutorial-01
+My first go at Django 1.6 via the tutorial on https://docs.djangoproject.com/en/1.6/intro/tutorial01/
+
---
+++ polls/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from polls.models import Poll
+
+def index(request):
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5] 
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_poll_list': latest_poll_list,
+    })
+    return HttpResponse(template.render(context))
+
+def detail(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of pull %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
+
