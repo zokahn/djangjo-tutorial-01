23df6247b8b7873b0887214883ae92045d3fa546
Bart van den Heuvel <bart@redhat.com>
2015-01-25 13:25:06 +0100

    Changed to generic views instead of views created seperately.

--- polls/views.py
+++ polls/views.py
@@ -3,25 +3,28 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 from django.http import HttpResponse, HttpResponseRedirect
-from django.template import RequestContext, loader
-from polls.models import Poll, Choice
 from django.core.urlresolvers import reverse
+from django.views import generic
 
-def index(request):
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5] 
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_poll_list': latest_poll_list,
-    })
-    return HttpResponse(template.render(context))
+#from django.template import RequestContext, loader
 
-def detail(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/detail.html', {'poll': poll})
+from polls.models import Poll, Choice
 
-def results(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/results.html', {'poll': poll})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
+
+    def get_queryset(self):
+        """Reurn the last published polls."""
+        return Poll.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
 
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
